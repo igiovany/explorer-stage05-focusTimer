d2da2dd19a71b43ffb5ef1124c4468b1ae4e1811
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2023-10-08 17:56:55 -0300

    JS - Fixing timeout accumulation bug

--- source/FocusTimer/timer.js
+++ source/FocusTimer/timer.js
@@ -5,6 +5,9 @@ import { kitchenTimer } from './sounds.js'
 
 
 export function countdown() {
+
+  clearTimeout(state.countdownId)
+  
   if (!state.isRunning) {
     return
   }
@@ -28,7 +31,7 @@ export function countdown() {
 
   updateDisplay(minutes, seconds)
 
-  setTimeout(() => countdown(), 1000)
+  state.countdownId = setTimeout(() => countdown(), 1000)
 }
 
 export function updateDisplay(minutes, seconds) {
